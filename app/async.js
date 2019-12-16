dd9affabd030ab0f9a523546c25c85f2c8fbc498
jevvilla <juan.es-19@hotmail.com>
2019-12-16 08:10:24 -0500

    js assessment solution

--- app/async.js
+++ app/async.js
@@ -2,12 +2,14 @@ asyncAnswers = {
   /**
    * Asynchronously returns a value via a promise. Example:
    * async('anyValue').then((result) => { return result === 'anyValue';});
-   * 
+   *
    * @param value - Any value
    * @returns {then: function} A promise like object containing a then property.
    */
   async: function async(value) {
-
+    return new Promise((resolve) => {
+      resolve(value);
+    });
   },
 
   /**
@@ -15,12 +17,23 @@ asyncAnswers = {
    * You may use jquery, XMLHttpRequest, or fetch.
    * https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
    * https://api.jquery.com/jQuery.ajax/
-   * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API   * 
-   * 
+   * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API   *
+   *
    * @param {String} url - a valid url
    * @returns {then: function} A promise like object containing a then property.
    */
-  manipulateRemoteData: function manipulateRemoteData(url) {
+  manipulateRemoteData: async function manipulateRemoteData(url) {
+    try {
+      const data = await fetch(url);
+      const result = await data.json();
 
+      const people = Object.values(result.people).map(item => item.name);
+
+      return new Promise((resolve) => {
+        resolve(people.sort());
+      });
+    } catch (error) {
+      return error;
+    }
   },
 };
--- app/recursion.js
+++ app/recursion.js
@@ -6,30 +6,44 @@ recursionAnswers = {
       dirName: 'app',
       files: ['index.html', 'page.html'],
       subDirs: [{...}]
-      }
+     }
    *
    * Where ... is the same type of object
-   * 
+   *
    * @param {fileSystemObject} data - a file system object as described above
    * @param {String} dirName - a directory name the files are desired to be listed from.
    * Note: This parameter is optional. If it is not provided, list ALL files.
-   * 
+   *
    * @returns {Number[]} The files under the directory dirName, including subdiretories.
    */
   listFiles: function listFiles(data, dirName) {
+    console.log(dirName);
+    if (data.dirName === dirName) {
+      console.log(data.files);
+    } else {
+      // console.log(data.files);
+    }
+    listFiles(data.subDirs);
+    // console.log(data);
+    // const files = [...listFiles(data.subDirs)];
 
+    // console.log(files);
+    // return data.file;
   },
 
   /**
    * Determines the fibonacci number at position n.
    * https://en.wikipedia.org/wiki/Fibonacci_number
-   * 
+   *
    * The first few fibonacci numbers are: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
-   * 
+   *
    * @param {Number} n - the index of the fibonacci number desired
    * @returns {Number} The nth fibonacci number
    */
   fibonacci: function fibonacci(n) {
-
+    if (n < 2) {
+      return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
   },
 };
